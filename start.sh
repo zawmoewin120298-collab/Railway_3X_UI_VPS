033ee19765980a8554e6011709eb374e076fa42a
zawmoewin120298-collab <zawmoewin120298@gmail.com>
2026-01-31 10:28:45 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 echo "Starting 3x-ui..."
-/usr/local/x-ui/x-ui > /dev/null 2>&1 &
+cd /usr/local/x-ui/
+./x-ui &
 
 sleep 5
 
-echo "Connecting to Cloudflare Tunnel..."
-# Token ကို ဒီထဲမှာ တိုက်ရိုက် ထည့်ပေးလိုက်တာပါ
+echo "Starting Cloudflare Tunnel..."
+# သင့်ရဲ့ Token ကို ဒီနေရာမှာ တိုက်ရိုက်ထည့်ပေးထားပါတယ်
 /usr/local/bin/cloudflared tunnel --no-autoupdate run --token eyJhIjoiOGQxZmJmOGJjOWM1NmFkOGYyMjZhMGM1MmNlZjgxNzIiLCJ0IjoiYWY4YzMwNWQtOWEwMC00YWEwLWFjNjMtNDVlY2Q2YjkwYjAyIiwicyI6IlpERmlNak0zTXpndE9UWTFOaTAwWm1ZNExXSmlOekl0TmpkalltUmpOV0ZoT1dObCJ9
