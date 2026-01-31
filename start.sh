677a7ca709a736bccacad0091a2cbed5066e6e7e
zawmoewin120298-collab <zawmoewin120298@gmail.com>
2026-01-31 09:41:09 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# 3x-ui service ကို စတင်မယ်
-/usr/local/x-ui/x-ui &
+echo "Starting 3x-ui..."
+/usr/local/x-ui/x-ui > /dev/null 2>&1 &
 
-echo "Waiting for 3x-ui to start..."
 sleep 5
 
-# Cloudflare Tunnel ကို run မယ်
-if [ -z "$TUNNEL_TOKEN" ]; then
-  echo "Error: TUNNEL_TOKEN variable is missing in Railway!"
-  exit 1
-fi
-
-echo "Starting Cloudflare Tunnel..."
-cloudflared tunnel --no-autoupdate run --token eyJhIjoiOGQxZmJmOGJjOWM1NmFkOGYyMjZhMGM1MmNlZjgxNzIiLCJ0IjoiYWY4YzMwNWQtOWEwMC00YWEwLWFjNjMtNDVlY2Q2YjkwYjAyIiwicyI6IlpERmlNak0zTXpndE9UWTFOaTAwWm1ZNExXSmlOekl0TmpkalltUmpOV0ZoT1dObCJ9
+echo "Connecting to Cloudflare Tunnel..."
+# Token ကို ဒီထဲမှာ တိုက်ရိုက် ထည့်ပေးလိုက်တာပါ
+/usr/local/bin/cloudflared tunnel --no-autoupdate run --token eyJhIjoiOGQxZmJmOGJjOWM1NmFkOGYyMjZhMGM1MmNlZjgxNzIiLCJ0IjoiYWY4YzMwNWQtOWEwMC00YWEwLWFjNjMtNDVlY2Q2YjkwYjAyIiwicyI6IlpERmlNak0zTXpndE9UWTFOaTAwWm1ZNExXSmlOekl0TmpkalltUmpOV0ZoT1dObCJ9
